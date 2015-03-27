40ebdae10bdccc2a26c226b54a898796f0ec9ef3
Gurvinder Singh <gurvinder.singh@uninett.no>
2015-03-27 17:45:18 +0100

    added command line options and removed some debugging

--- mgfspark.py
+++ mgfspark.py
@@ -1,11 +1,18 @@
 from pyspark import SparkContext
 from pyspark.conf import SparkConf
 from utils import parseMGF
+import argparse
 
-conf = SparkConf()
-conf.setAppName("MGF test").set("spark.executor.memory", "10g")
-sc = SparkContext(conf=conf)
-path = "hdfs://daas/promec/mgf"
+DESCRIPTION = "MGF data analysis using Apache Spark"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('path', help="Directory path to MGF files")
+    parser.add_argument('-p', '--partitions', default="60", type=int,
+                        help="Number of partitions to be used for processing")
+
+    return parser.parse_args()
 
 
 def process(raw_data):
@@ -13,9 +20,13 @@ def process(raw_data):
     return mgf_data
 
 
-raw_data = sc.wholeTextFiles(path, use_unicode=False, minPartitions=40)
-mgf_data = process(raw_data)
-# mgf_data.repartition(40)
-print mgf_data.take(1)
-print mgf_data.count()
-#print(raw_data.count())
+if __name__ == '__main__':
+    args = parse_args()
+    conf = SparkConf()
+    conf.setAppName("MGF test").set("spark.executor.memory", "10g")
+    sc = SparkContext(conf=conf)
+    raw_data = sc.wholeTextFiles(args.path, use_unicode=False, minPartitions=args.partitions)
+    mgf_data = process(raw_data).repartition(args.partitions)
+    print mgf_data.take(1)
+    # print mgf_data.take(1)[0]['params']
+    print mgf_data.count()
